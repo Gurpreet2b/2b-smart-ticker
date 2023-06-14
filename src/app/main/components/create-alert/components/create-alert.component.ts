3c3b241b6abcca0abcd23b15babaaada394cfb57
Gurpreet Singh <gurpreets@2binnovations.com>
2023-06-14 15:20:20 +0530

    issue fixed

--- src/app/main/components/create-alert/components/create-alert.component.ts
+++ src/app/main/components/create-alert/components/create-alert.component.ts
@@ -433,6 +433,7 @@ export class CreateAlertComponent implements OnInit {
           teams_alert: false,
         });
         this.AlertEditTitlePreview = res.data.name;
+        this.AlertEditTitlePreviewName = res.data.name;
         await new Promise(f => setTimeout(f, 2000));
         this.IsScript();
         this.authService.setCurrentUser({ token: res.token });
